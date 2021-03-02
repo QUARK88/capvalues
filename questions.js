a51d663573df258fae68f5fd58b6486d6dd0bf80
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-02 12:08:23 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,19 @@
+questions = [
+    {
+        "question": ".",
+        "effect": {
+            "econ": 0,
+            "soci": 0,
+            "poli": 0,
+            "stat": 0,
+            "indu": 0,
+            "econ": 0,
+            "soci": 0,
+            "poli": 0,
+            "stat": 0,
+            "indu": 0,
+            "stat": 0,
+            "indu": 0
+        }
+    },
+];
