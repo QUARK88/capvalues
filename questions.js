6c228760f1b00ac2dbb426ae39563e7c9096a4b9
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-02 12:22:02 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,13 +7,13 @@ questions = [
             "poli": 0,
             "stat": 0,
             "indu": 0,
-            "econ": 0,
-            "soci": 0,
-            "poli": 0,
-            "stat": 0,
-            "indu": 0,
-            "stat": 0,
-            "indu": 0
+            "land": 0,
+            "reli": 0,
+            "nati": 0,
+            "inte": 0,
+            "scie": 0,
+            "elit": 0,
+            "unit": 0
         }
     }
 ];
