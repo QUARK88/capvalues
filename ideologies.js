ce1d0f95375ec41a768677f3e60ce40a6346b58d
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-05 18:27:41 -0500

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,53 @@
+ideologies = [
+    {
+        "name": "Quarkism",
+        "stats": {
+            "becon": 5,
+            "bsoci": 65,
+            "bpoli": 20,
+            "bstat": 60,
+            "bindu": 30,
+            "bland": 70,
+            "breli": 100,
+            "bnati": 20,
+            "binte": 0,
+            "bscie": 100,
+            "belit": 10,
+            "bunit": 35
+        }
+    },
+    {
+        "name": "Centrism",
+        "stats": {
+            "becon": 50,
+            "bsoci": 50,
+            "bpoli": 50,
+            "bstat": 50,
+            "bindu": 50,
+            "bland": 50,
+            "breli": 50,
+            "bnati": 50,
+            "binte": 50,
+            "bscie": 50,
+            "belit": 50,
+            "bunit": 50
+        }
+    },
+    {
+        "name": "Nyuffdasha",
+        "stats": {
+            "becon": 85,
+            "bsoci": 75,
+            "bpoli": 85,
+            "bstat": 35,
+            "bindu": 25,
+            "bland": 100,
+            "breli": 100,
+            "bnati": 40,
+            "binte": 100,
+            "bscie": 85,
+            "belit": 100,
+            "bunit": 0
+        }
+    }
+];
